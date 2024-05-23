1693af72247c7bedd83a789f32c815a706da402e
sou10202 <souichi.12rou14@gmail.com>
2024-05-23 23:38:21 +0900

    ganma

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
 import imageio
 from universe import Universe
+import yaml
 
 def main():
+  with open('config/walker_config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+  ganma = config['ganma']
   data = []
   universe = Universe()
   universe.univ_init()
-  universe.plot_hist(3)
+  universe.plot_hist(ganma)
   for day in range(universe.Days):
     universe.univ_step_begin()
     for one in universe.walkers:
@@ -14,10 +18,10 @@ def main():
     universe.univ_step_end()
     data.append(universe.Rate_SIR[:])
     universe.plot_walkers(day)
-  universe.plot_data(data, 3)
+  universe.plot_data(data,ganma)
 
     # Create GIF
-  with imageio.get_writer('simulation.gif', mode='I', duration=0.5) as writer:
+  with imageio.get_writer(f'/out/GIF/simulation_ganma{ganma}.gif', mode='I', duration=0.5) as writer:
       for day in range(universe.Days):
           filename = f'frames/frame_{day}.png'
           image = imageio.imread(filename)
